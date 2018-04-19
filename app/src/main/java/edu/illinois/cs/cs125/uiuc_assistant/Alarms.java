8bed78c5fb9c9b86ae7cce57eee9af8bf84072e8
Amanthebest4 <amana3@illinois.edu>
2018-04-19 18:04:54 -0500

    tabs inserted

---
+++ app/src/main/java/edu/illinois/cs/cs125/uiuc_assistant/Alarms.java
@@ -0,0 +1,18 @@
+package edu.illinois.cs.cs125.uiuc_assistant;
+
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Alarms extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.alarms, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/edu/illinois/cs/cs125/uiuc_assistant/Home.java
@@ -0,0 +1,21 @@
+package edu.illinois.cs.cs125.uiuc_assistant;
+
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment{
+    private static final String TAG = "home";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home, container, false);
+        return rootView;
+    }
+}
