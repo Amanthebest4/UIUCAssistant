ec18e8fcc2b0f31a1c472dd21c079c30e354dc3e
Amanthebest4 <amana3@illinois.edu>
2018-04-19 22:09:25 -0500

    map working with manual permissions

--- app/src/main/java/edu/illinois/cs/cs125/uiuc_assistant/MapTab.java
+++ app/src/main/java/edu/illinois/cs/cs125/uiuc_assistant/MapTab.java
@@ -4,13 +4,19 @@ package edu.illinois.cs.cs125.uiuc_assistant;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,7 +24,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapTab extends Fragment implements OnMapReadyCallback {
+
+    private Button search;
+    private FloatingActionButton zoomIn;
+    private FloatingActionButton zoomOut;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +41,29 @@ public class MapTab extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        search = (Button) rootView.findViewById(R.id.BSearch);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSearch(v);
+            }
+        });
+
+        zoomIn = (FloatingActionButton) rootView.findViewById(R.id.zoom_in);
+        zoomOut = (FloatingActionButton) rootView.findViewById(R.id.zoom_out);
+        zoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomInMethod();
+            }
+        });
+        zoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomOutMethod();
+            }
+        });
 
         return rootView;
     }
@@ -56,7 +92,7 @@ public class MapTab extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -64,7 +100,9 @@ public class MapTab extends Fragment implements OnMapReadyCallback {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            //mMap.setMyLocationEnabled(true);
+
+        } else {
+            mMap.setMyLocationEnabled(true);
         }
 
 
@@ -72,5 +110,33 @@ public class MapTab extends Fragment implements OnMapReadyCallback {
         LatLng champaign = new LatLng(40.1070, -88.2272);
         mMap.addMarker(new MarkerOptions().position(champaign).title("Marker at the university of Illinois at Urbana-Champaign"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(champaign));
+    }
+
+    public void onSearch(View view) {
+        EditText location_TF = (EditText) getActivity().findViewById(R.id.TFadress);
+        String location = location_TF.getText().toString();
+
+        List<Address> addressList = null;
+        if (location != null && !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this.getActivity());
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker for search result"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
+    private void zoomInMethod() {
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+    }
+    private void zoomOutMethod() {
+        mMap.animateCamera(CameraUpdateFactory.zoomOut());
     }
 }
